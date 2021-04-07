6e66c700384acd71904088e7735ae00879976033
Aditya Kumar <73995639+ZZZZ-afk@users.noreply.github.com>
2021-04-07 12:44:33 +0530

    Add files via upload

---
+++ Bob.js
@@ -0,0 +1,27 @@
+class Bob{
+  constructor(x, y, radius){
+    var options={
+        isStatic:false,
+        restitution:1,
+        friction:0,
+        density:0.8
+    }
+      this.body = Matter.Bodies.circle(x, y, radius,options)
+      this.radius = radius;
+      World.add(world, this.body);
+  }
+
+  display(){
+     var pos =this.body.position;
+     var angle = this.body.angle;
+     push();
+     translate(pos.x, pos.y);
+     rotate(angle);
+     ellipseMode(RADIUS)
+     strokeWeight(2);
+     stroke(0);
+     fill("pink");
+     ellipse(0,0,this.radius);
+     pop();
+  }
+}
